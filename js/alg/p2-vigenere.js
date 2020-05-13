7b3c5f4e57cfc517bf9ba2de739a299ebec005b5
dkuroi <kaerit@hotmail.com>
2020-05-13 12:24:18 +0100

    Updates

---
+++ js/alg/p2-vigenere.js
@@ -0,0 +1,4 @@
+export default class p2 {
+    constructor(cipher_key, Plaintext) {
+    }
+}
---
+++ js/utils/transforms.js
@@ -0,0 +1,81 @@
+var ints = {
+    fromString(msg) {
+        return msg.split('').map((char) => {
+            return char.charCodeAt(0);
+        });
+    },
+
+    fromHexs(arr_hex) {
+        if(!Array.isArray(arr_hex)) return parseInt(arr_hex,16);
+        else {
+            return arr_hex.map((byte) => {
+                return parseInt(byte,16);
+            });
+        }
+    },
+
+    fromBins(arr_bin) {
+        if(!Array.isArray(arr_bin)) return parseInt(arr_bin,2);
+        else {
+            return arr_bin.map((byte) => {
+                return parseInt(byte,2);
+            });
+        }
+    }
+}
+
+var hexs = {
+    fromInts(arr_int, mode) {
+        if(!Array.isArray(arr_int)) {
+            if(mode == "number") {
+                if(arr_int.toString(16).length < 2) return Number(`0x0${arr_int.toString(16)}`);
+                else                                return Number(`0x${arr_int.toString(16)}`);
+            } else {
+                if(arr_int.toString(16).length < 2) return (`0${arr_int.toString(16)}`);
+                else                                return (`${arr_int.toString(16)}`);
+            }
+            
+        } 
+        else {
+            return arr_int.map((byte) => {
+                if(byte.toString(16).length < 2) return "0"+byte.toString(16);
+                else                             return byte.toString(16);
+            });
+        }
+
+    },
+
+    fromString(msg)   { return this.fromInts(ints.fromString(msg));   },
+    fromBins(arr_bin) { return this.fromInts(ints.fromBins(arr_bin)); }
+}
+
+
+var bins = {
+    fromInts(arr_int) {
+        if(!Array.isArray(arr_int)) return "0".repeat(8 - arr_int.toString(2).length) + arr_int.toString(2);
+        else {
+            return arr_int.map((byte) => {
+                return "0".repeat(8 - byte.toString(2).length) + byte.toString(2);
+            });
+        } 
+    },
+
+    fromString(msg)   { return this.fromInts(ints.fromString(msg));   },
+    fromHexs(arr_hex) { return this.fromInts(ints.fromHexs(arr_hex)); }
+}
+
+var str  ={
+    fromInts(arr_int) {
+        if(!Array.isArray(arr_int)) return String.fromCharCode(arr_int);
+        else {
+            return arr_int.map((byte,i) => {
+                return String.fromCharCode(byte);
+            });
+        }     
+    },
+
+    fromHexs(arr_hex) { return this.fromInts(ints.fromHexs(arr_hex)); },
+    fromBins(arr_bin) { return this.fromInts(ints.fromBins(arr_bin)); }
+}
+
+export { ints, hexs, bins, str }
